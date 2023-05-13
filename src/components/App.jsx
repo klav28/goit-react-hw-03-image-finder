59e97e86c61b1a3e1cd35497e10a4992709d8ddd
Konstantin Leschenko <klav28@gmail.com>
2023-05-13 20:45:14 +0300

    initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 30,
+        color: '#010101',
+      }}
+    >
+      <p>Hello Image Finder</p>
+    </div>
+  );
+};
